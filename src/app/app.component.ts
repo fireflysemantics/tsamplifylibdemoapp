ccb92316994b2bc5d3ed45c7f851ec61a1022c5f
Ole Ersoy <ole.ersoy@gmail.com>
2022-02-04 16:18:54 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { getCurrentAuthenticatedUserAttributes} from 'amplifylibe'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
